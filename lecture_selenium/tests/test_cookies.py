909c35037e35fd6c7f3a2e8bce6bb08e79d041de
ASVARYCH <asvarych@luxoft.com>
2023-05-26 17:54:12 +0300

    added test with cookies

--- lecture_selenium/tests/test_cookies.py
+++ lecture_selenium/tests/test_cookies.py
@@ -6,23 +6,26 @@ from selenium.webdriver.common.by import By
 
 def test_cookies(chrome):
     driver = chrome
-    # Відкриваємо файл збережених cookies
-    with open('./cookies.pkl', 'rb') as file:
+    driver.get('https://rozetka.com.ua/')
+    with open('cookies.pkl', 'rb') as file:
         cookies = pickle.load(file)
-    # Додаємо cookies до веб-драйвера
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-
-    driver.get('https://rozetka.com.ua/ua/')
-    user_button = driver.find_element(By.XPATH, '(//button[contains(@class, "header__button")])[2]')
-    user_button.click()
-    _ = driver.find_element(By.CSS_SELECTOR, 'div.modal__holder')
-    email_field = driver.find_element(By.CSS_SELECTOR, 'input[type="email"]')
-    password_field = driver.find_element(By.CSS_SELECTOR, 'input#auth_pass')
-    email_field.send_keys('hilltober@gmail.com')
-    password_field.send_keys('Gfhjkm123')
-    remember_checkbox = driver.find_element(By.CSS_SELECTOR, 'label[for="remember_me"]')
-    remember_checkbox.click()
-    submit_button = driver.find_element(By.XPATH, '//button[contains(@class, "auth-modal__submit")]')
-    submit_button.click()
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+    driver.refresh()
     pass
+    # user_button = driver.find_element(By.XPATH, '(//button[contains(@class, "header__button")])[2]')
+    # user_button.click()
+    # _ = driver.find_element(By.CSS_SELECTOR, 'div.modal__holder')
+    # email_field = driver.find_element(By.CSS_SELECTOR, 'input[type="email"]')
+    # password_field = driver.find_element(By.CSS_SELECTOR, 'input#auth_pass')
+    # email_field.send_keys('hilltober@gmail.com')
+    # password_field.send_keys('Gfhjkm123')
+    # remember_checkbox = driver.find_element(By.CSS_SELECTOR, 'label[for="remember_me"]')
+    # remember_checkbox.click()
+    # submit_button = driver.find_element(By.XPATH, '//button[contains(@class, "auth-modal__submit")]')
+    # submit_button.click()
+    # # cookies = driver.get_cookies()
+    # # Зберігаємо cookies у файл за допомогою pickle
+    # # with open('cookies.pkl', 'wb') as file:
+    # #     pickle.dump(cookies, file)
+    # pass
