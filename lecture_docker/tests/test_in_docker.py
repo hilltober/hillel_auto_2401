4afaa8a79a51120b739035d88ac02e3ddf45bfb7
hilltober <hilltober@gmail.com>
2023-06-11 02:25:35 +0300

    test 12

--- lecture_docker/tests/test_in_docker.py
+++ lecture_docker/tests/test_in_docker.py
@@ -1,10 +1,10 @@
 class TestDocker:
 
     def test_docker1(self):
-        assert True
+        assert False
 
     def test_docker2(self):
-        assert True
+        assert False
 
     def test_docker3(self):
         assert False
